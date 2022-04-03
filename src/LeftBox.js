4b2824cf492d4428b3ee0baf0e7f7e7d20eb71d2
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-04-03 20:50:20 +0530

    first commit

---
+++ src/LeftBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LeftBox = () => {
+    return (<>
+        <div className="side1">
+            <div className='datebox'>LeftBox </div>
+            <div className='searchbox'>LeftBox </div>
+            <div className='motobox'>LeftBox </div>
+        </div>
+        <div className="side2">
+            <div className='moreon'></div>
+        </div>
+    </>
+    )
+}
+
+export default LeftBox
---
+++ src/RightBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightBox = () => {
+  return (
+    <div>RightBox</div>
+  )
+}
+
+export default RightBox
